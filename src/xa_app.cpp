5049c929ebaf80af20465563f2681bfd9cf4d940
Gunther Laure <gunther.laure@gmail.com>
2024-12-30 19:06:48 +0100

    Ubuntu 24.04 force light mode

--- src/xa_app.cpp
+++ src/xa_app.cpp
@@ -27,6 +27,74 @@
 #include <QPalette>
 #include <QStyleHints>
 
+
+namespace
+{
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 5, 0)
+QPalette qt_fusionPalette_legacy(bool darkAppearance)
+{
+    // auto theme = QGuiApplicationPrivate::platformTheme();
+    // const bool darkAppearance = theme
+    //                           ? theme->colorScheme() == Qt::ColorScheme::Dark
+    //                           : false;
+    //const bool darkAppearance = false;
+
+    const QColor windowText = darkAppearance ? QColor(240, 240, 240) : Qt::black;
+    const QColor backGround = darkAppearance ? QColor(50, 50, 50) : QColor(239, 239, 239);
+    const QColor light = backGround.lighter(150);
+    const QColor mid = (backGround.darker(130));
+    const QColor midLight = mid.lighter(110);
+    const QColor base = darkAppearance ? backGround.darker(140) : Qt::white;
+    const QColor disabledBase(backGround);
+    const QColor dark = backGround.darker(150);
+    const QColor darkDisabled = QColor(209, 209, 209).darker(110);
+    const QColor text = darkAppearance ? windowText : Qt::black;
+    const QColor highlight = QColor(48, 140, 198);
+    const QColor hightlightedText = darkAppearance ? windowText : Qt::white;
+    const QColor disabledText = darkAppearance ? QColor(130, 130, 130) : QColor(190, 190, 190);
+    const QColor button = backGround;
+    const QColor shadow = dark.darker(135);
+    const QColor disabledShadow = shadow.lighter(150);
+    const QColor disabledHighlight(145, 145, 145);
+    QColor placeholder = text;
+    placeholder.setAlpha(128);
+
+    QPalette fusionPalette(windowText, backGround, light, dark, mid, text, base);
+    fusionPalette.setBrush(QPalette::Midlight, midLight);
+    fusionPalette.setBrush(QPalette::Button, button);
+    fusionPalette.setBrush(QPalette::Shadow, shadow);
+    fusionPalette.setBrush(QPalette::HighlightedText, hightlightedText);
+
+    fusionPalette.setBrush(QPalette::Disabled, QPalette::Text, disabledText);
+    fusionPalette.setBrush(QPalette::Disabled, QPalette::WindowText, disabledText);
+    fusionPalette.setBrush(QPalette::Disabled, QPalette::ButtonText, disabledText);
+    fusionPalette.setBrush(QPalette::Disabled, QPalette::Base, disabledBase);
+    fusionPalette.setBrush(QPalette::Disabled, QPalette::Dark, darkDisabled);
+    fusionPalette.setBrush(QPalette::Disabled, QPalette::Shadow, disabledShadow);
+
+    fusionPalette.setBrush(QPalette::Active, QPalette::Highlight, highlight);
+    fusionPalette.setBrush(QPalette::Inactive, QPalette::Highlight, highlight);
+    fusionPalette.setBrush(QPalette::Disabled, QPalette::Highlight, disabledHighlight);
+
+    // fusionPalette.setBrush(QPalette::Active, QPalette::Accent, highlight);
+    // fusionPalette.setBrush(QPalette::Inactive, QPalette::Accent, highlight);
+    // fusionPalette.setBrush(QPalette::Disabled, QPalette::Accent, disabledHighlight);
+
+    fusionPalette.setBrush(QPalette::PlaceholderText, placeholder);
+
+    // Use a more legible light blue on dark backgrounds than the default Qt::blue.
+    if (darkAppearance)
+        fusionPalette.setBrush(QPalette::Link, highlight);
+
+    return fusionPalette;
+}
+#endif
+
+}
+
+
+
 XAApp::XAApp(int& argc, char** argv)
     : QObject(nullptr)
     , m_app(argc, argv)
@@ -76,13 +144,21 @@ bool XAApp::isDarkMode() const
 #else
     const QPalette defaultPalette;
     const auto text = defaultPalette.color(QPalette::WindowText);
+    const auto window = defaultPalette.color(QPalette::Window);
     return text.lightness() > window.lightness();
 #endif // QT_VERSION
 }
 
+
 void XAApp::selectColorTheme(const QString& color_theme)
 {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
     QGuiApplication::styleHints()->setColorScheme(Qt::ColorScheme::Light);
+#else
+    auto app = qobject_cast<QApplication*>(QApplication::instance());
+    app->setPalette(qt_fusionPalette_legacy(false));
+#endif // QT_VERSION
+
 
     //auto app = qobject_cast<QApplication*>(QApplication::instance());
     //if (app)
@@ -113,3 +189,4 @@ int XAApp::run()
 
     return ret;
 }
+
